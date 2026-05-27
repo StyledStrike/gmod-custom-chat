79ea15f8c029b718ce9ffd17d5936e4bd5bd35e8
StyledStrike <lameulucas@gmail.com>
2026-05-26 22:21:37 -0300

    Include all players (even bots) on customchat.say

--- lua/custom_chat/server/net_messages.lua
+++ lua/custom_chat/server/net_messages.lua
@@ -4,7 +4,7 @@ util.AddNetworkString( "customchat.say" )
 -- listen to messages from a "speaker".
 local function GetListeners( speaker, text, channel )
     local teamOnly = channel == "team"
-    local targets = teamOnly and team.GetPlayers( speaker:Team() ) or player.GetHumans()
+    local targets = teamOnly and team.GetPlayers( speaker:Team() ) or player.GetAll()
     local listeners = {}
 
     for _, ply in ipairs( targets ) do
